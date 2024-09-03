485004237b8bd8ae65a5a86bf2a7feeefbd39170
ratanawnk <sanratana18@gmail.com>
2024-09-03 16:33:19 +0700

    add Particle backgruond

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import ExperienceSection from "./components/ExperienceSection";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import ParticleBackground from "./components/ParticleBackground";
 
 function App() {
   return (
     <div className="bg-gray-800">
+      <ParticleBackground />
       <div className="container mx-auto min-h-screen px-4">
         <Header />
         <Hero />
